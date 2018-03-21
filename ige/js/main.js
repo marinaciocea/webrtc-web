a1c32529fb07673d76bd8c375449e5783bc31ed0
Marina Ciocea <marinaciocea@chromium.org>
2018-03-21 11:29:26 +0100

    Update filters.

--- ige/js/main.js
+++ ige/js/main.js
@@ -14,48 +14,31 @@ var snapBtn = document.getElementById('snap');
 var videoFilter;
 
 // Attach event handlers
-document.getElementById('green').addEventListener('click', setGreenFilter);
 document.getElementById('blue').addEventListener('click', setBlueFilter);
-document.getElementById('pink').addEventListener('click', setPinkFilter);
-document.getElementById('yellow').addEventListener('click', setYellowFilter);
-document.getElementById('blur').addEventListener('click', setBlurFilter);
 document.getElementById('clear').addEventListener('click', setClearFilter);
 
-snapBtn.addEventListener('click', snapPhoto);
+// snapBtn.addEventListener('click', snapPhoto);
 
-function setVideoFiler(filter) {
+function setVideoFilter(filter) {
     videoFilter = filter;
     document.getElementById('camera').style.filter = filter;
 }
 
-function setGreenFilter() {
-    setVideoFiler('hue-rotate(90deg)  saturate(200%)');
-}
-
 function setBlueFilter() {
-    setVideoFiler('hue-rotate(180deg)  saturate(200%)');
-}
-
-function setPinkFilter() {
-    setVideoFiler('hue-rotate(270deg)  saturate(200%)');
-}
-
-function setYellowFilter() {
-    setVideoFiler('hue-rotate(360deg)  saturate(200%)');
-}
-
-function setBlurFilter() {
-    setVideoFiler('blur(4px) invert(1) opacity(0.5)');
+    setVideoFilter('hue-rotate(180deg)  saturate(200%)');
 }
 
 function setClearFilter() {
-    setVideoFiler('');
+    setVideoFilter('');
 }
 
+/*
+// Capture current snapshot of video stream and display into photo
 function snapPhoto() {
     photoContext.drawImage(video, 0, 0, photo.width, photo.height);
     document.getElementById('photo').style.filter = videoFilter;
 }
+*/
 
 // Handles success by adding the MediaStream to the video element.
 function gotLocalMediaStream(mediaStream) {
